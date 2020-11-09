0e924f46e7fd1e5ac44e866434cb3d6d781ebaf6
Giulia Straccali <straccaligiulia@gmail.com>
2020-11-09 11:21:53 +0100

    Prima bozza dimensioni calcolo di un'immagine

---
+++ DimensioniCalcoloImmagine/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DimensioniCalcoloImmagine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dimensione1;
+            int dimensione2;
+            
+            Console.WriteLine("inserisci la prima dimensione");
+            dimensione1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Inserisci la seconda dimensione");
+            dimensione2 = Convert.ToInt32(Console.ReadLine());
+
+            int pixel = (dimensione1 * dimensione2) *3 / 1024; 
+            Console.WriteLine($"Il peso dell'immagine è {pixel}KB");
+            Console.ReadKey();
+
+           
+        }
+    }
+}
