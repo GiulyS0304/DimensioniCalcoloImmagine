2cf6d762f737427c33a730ed6fa947e3309f7848
Giulia Straccali <straccaligiulia@gmail.com>
2020-11-09 13:01:45 +0100

    Esercizio funzionante dimensione calcolo immagine

--- DimensioniCalcoloImmagine/Program.cs
+++ DimensioniCalcoloImmagine/Program.cs
@@ -8,18 +8,38 @@ namespace DimensioniCalcoloImmagine
         {
             int dimensione1;
             int dimensione2;
-            
-            Console.WriteLine("inserisci la prima dimensione");
+            int operazione;
+            string bit;
+
+            Console.WriteLine("inserisci la prima dimensione dell'immagine");
             dimensione1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Inserisci la seconda dimensione");
-            dimensione2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Inserisci la seconda dimensione dell'immagine");
+            dimensione2 = Convert.ToInt32(Console.ReadLine());        
+            
+            Console.WriteLine("Inserisci da quanti bit è formata l'immagine");
+            bit = (Console.ReadLine());
 
-            int pixel = (dimensione1 * dimensione2) *3 / 1024; 
-            Console.WriteLine($"Il peso dell'immagine è {pixel}KB");
+            switch (bit)
+            {
+                case "8":
+                    operazione = (dimensione1 * dimensione2)  / 1024;
+                    Console.WriteLine($"Il peso dell'immagine è: {operazione}KB");
+                    break;
+
+                case "16":
+                    operazione = (dimensione1 * dimensione2) * 2 / 1024;
+                    Console.WriteLine($"Il peso dell'immagine è: {operazione}KB");
+                    break;
+
+                case "24":
+                    operazione = (dimensione1 * dimensione2) * 3 / 1024;
+                    Console.WriteLine($"Il peso dell'immagine è: {operazione}KB");
+                    break;
+            }
             Console.ReadKey();
 
-           
+
         }
     }
 }
